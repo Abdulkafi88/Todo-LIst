c982cd48ef1ee3219900667828b12ed28aa6ed1f
Abdulkafi88 <123754218+Abdulkafi88@users.noreply.github.com>
2023-02-10 16:28:35 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,20 +1,46 @@
-function todo(){
-let newp=document.createElement('p')
-let value=document.getElementById('input').value
-let addTextNode=document.createTextNode(value)
-newp.appendChild(addTextNode)
-newp.addEventListener('click',function(){
-    newp.style.textDecoration='line-through'
-})
-newp.addEventListener('dblclick',function(){
-    newp.removeChild(addTextNode)
-})
-if(value===''){
-    alert('add new Task')
-}
-else{
-    document.getElementById('list').appendChild(newp)
+// function todo(){
+// let newp=document.createElement('p')
+// let value=document.getElementById('input').value
+// let addTextNode=document.createTextNode(value)
+// newp.appendChild(addTextNode)
+// newp.addEventListener('click',function(){
+//     newp.style.textDecoration='line-through'
+// })
+// newp.addEventListener('dblclick',function(){
+//     newp.removeChild(addTextNode)
+// })
+// if(value===''){
+//     alert('add new Task')
+// }
+// else{
+//     document.getElementById('list').appendChild(newp)
     
+// }
+// document.getElementById('input').value=''
+// }
+
+// same thing 
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
-document.getElementById('input').value=''
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
+
+
+
+
